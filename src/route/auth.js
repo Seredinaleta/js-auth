a95e6b4cf6bfed79f914196489b6cea5b50b3ed9
Bill Gates <yuliia.trubina.qa@gmail.com>
2023-11-13 15:45:07 +0200

    Autentification- recovery function done

--- src/route/auth.js
+++ src/route/auth.js
@@ -4,6 +4,8 @@ const express = require('express')
 const router = express.Router()
 
 const { User } = require('../class/user')
+const { Confirm } = require('../class/confirm')
+
 User.create({
   email: 'user@mail.com',
   password: 123,
@@ -19,7 +21,7 @@ router.get('/signup', function (req, res) {
   // res.render генерує нам HTML сторінку
 
   // ↙️ cюди вводимо назву файлу з сontainer
-  res.render('signup', {
+  return res.render('signup', {
     // вказуємо назву контейнера
     name: 'signup',
     // вказуємо назву компонентів
@@ -52,7 +54,7 @@ router.get('/signup', function (req, res) {
   })
   // ↑↑ сюди вводимо JSON дані
 })
-
+// ================================================================
 router.post('/signup', function (req, res) {
   const { email, password, role } = req.body
   console.log(req.body)
@@ -64,7 +66,18 @@ router.post('/signup', function (req, res) {
   }
 
   try {
+    const user = User.getByEmail(email)
+    console.log(user)
+
+    if (user) {
+      return res.status(400).json({
+        message:
+          'Помилка. Користувач з таким email вже існує',
+      })
+    }
+
     User.create({ email, password, role })
+    // const newUser = User.create({ email, password, role })
 
     return res.status(200).json({
       message: `Користувач успішно зареєстрований`,
@@ -75,6 +88,122 @@ router.post('/signup', function (req, res) {
     })
   }
 })
+// ================================================================
 
+// router.get Створює нам один ентпоїнт
+
+// ↙️ тут вводимо шлях (PATH) до сторінки
+router.get('/recovery', function (req, res) {
+  // res.render генерує нам HTML сторінку
+
+  // ↙️ cюди вводимо назву файлу з сontainer
+  return res.render('recovery', {
+    // вказуємо назву контейнера
+    name: 'recovery',
+    // вказуємо назву компонентів
+    component: ['back-button', 'field'],
+
+    // вказуємо назву сторінки
+    title: 'Recovery page',
+    // ... сюди можна далі продовжувати додавати потрібні технічні дані, які будуть використовуватися в layout
+
+    // вказуємо дані,
+    data: {},
+  })
+  // ↑↑ сюди вводимо JSON дані
+})
+// ================================================================
+router.post('/recovery', function (req, res) {
+  const { email } = req.body
+  console.log(email)
+
+  if (!email) {
+    return res.status(400).json({
+      message: "Помилка. Обов'язкові поля відсутні",
+    })
+  }
+
+  try {
+    const user = User.getByEmail(email)
+    // console.log(user)
+
+    if (!user) {
+      return res.status(400).json({
+        message:
+          'Помилка. Користувача з таким email не існує',
+      })
+    }
+
+    Confirm.create(email)
+    return res.status(200).json({
+      message: 'Код відновлення паролю відправлено',
+    })
+  } catch (err) {
+    return res.status(400).json({
+      message: err.message,
+    })
+  }
+})
+// ================================================================
+
+// router.get Створює нам один ентпоїнт
+
+// ↙️ тут вводимо шлях (PATH) до сторінки
+router.get('/recovery-confirm', function (req, res) {
+  // res.render генерує нам HTML сторінку
+
+  // ↙️ cюди вводимо назву файлу з сontainer
+  return res.render('recovery-confirm', {
+    // вказуємо назву контейнера
+    name: 'recovery-confirm',
+    // вказуємо назву компонентів
+    component: ['back-button', 'field', 'field-password'],
+
+    // вказуємо назву сторінки
+    title: 'Recovery confirm page',
+    // ... сюди можна далі продовжувати додавати потрібні технічні дані, які будуть використовуватися в layout
+
+    // вказуємо дані,
+    data: {},
+  })
+  // ↑↑ сюди вводимо JSON дані
+})
+// ================================================================
+router.post('/recovery-confirm', function (req, res) {
+  const { password, code } = req.body
+  console.log(password, code)
+
+  if (!code || !password) {
+    return res.status(400).json({
+      message: `Помилка. Відсутні обов'язкові дані`,
+    })
+  }
+  try {
+    const email = Confirm.getData(Number(code))
+
+    if (!email) {
+      return res.status(400).json({
+        message: 'Код не існує',
+      })
+    }
+    const user = User.getByEmail(email)
+    if (!user) {
+      return res.status(400).json({
+        message:
+          'Користувача з таким e-mail не зареєстровано',
+      })
+    }
+    user.password = password
+    console.log(user)
+
+    return res.status(200).json({
+      message: 'Пароль успішно змінено',
+    })
+  } catch (err) {
+    return res.status(400).json({
+      message: err.message,
+    })
+  }
+})
 // Підключаємо роутер до бек-енду
 module.exports = router
